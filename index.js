ece5bbf57416a1d9d2761fd7c89b3308a927d6b4
Anjitha5559 <84130915+Anjitha5559@users.noreply.github.com>
2023-02-21 23:55:37 +0530

    Anjitha

--- index.js
+++ index.js
@@ -27,17 +27,43 @@ const items = [
 
 
 //To pick random items from the items array
-function generateRandom(){
-  let tempArray = [...items];
+function generateRandom(size = 4) {
+  let tempArray = [...items];//Just copying the array as elements to tempArray
   console.log(tempArray);
-  let randomArray = [];
-  size = (size*size)/2;
-  for(let i=0;i<size;i++){
-    const randomIndex = Math.floor(math.random())*size;
-    randomArray.push(tempArray[randomIndex]);
-    tempArray.splice(randomIndex,1);
+  let randomArray = [];//Just initiating the array in which the randomly chosen elements are stored
+  size = (size*size)/2;//This size is so cuz if we chose 4*4 matrix then we have 16 slots and we need 8 images as each repeats one time 
+  for(let i=0;i<size;i++){//making a loop to pick random elements
+    const randomIndex = Math.floor(Math.random()*tempArray.length);//math.random makes a random value bw 0 and 1 multiplying it with the size of tempArray and then rounding it will give different locations which is stores in LHS
+    randomArray.push(tempArray[randomIndex]);//Inserting the randomly chosen elemetn to randomArray
+    tempArray.splice(randomIndex,1);//splicing it from tempArray so that it is not repeated
   }
+  console.log(randomArray);//This is not printing so itinr munne tanne error nd
+
+return randomArray;//returning the array
+}
+matrixGenerator();
+
+//converting the array to matrix format
+function matrixGenerator(randomArray,size=4){
+  gameBody.innerHTML="";
+  randomArray = [...randomArray,...randomArray];//This is done coz the randomly chosen images has to be repeated twice
+  //doing shuffling
+  randomArray.sort(() => Math.random() - 0.5);
+  for(let i=0;i<size;i++){
+    gameBody.innerHTML+=`
+    <div class="slots" slotValue = "${randomArray[i].name}">
+    <div class="card-before">?</div>
+        <div class="card-after">
+        <img src="img/${randomArray[i].image}" class="image"/></div>
+    </div>`
+  }
+game.style.gridTemplateColumns = `repeat(${size},1fr)`;
+
 
 }
-
+const initializer = () => {
+  let randomArray = generateRandom();
+  console.log(tempArray);
+  matrixGenerator(tempArray);
+};
 
